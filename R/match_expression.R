f6da76a47bff4ffdebfb083bf4f6b64abf910e7e
Aviral Goel <goel.aviral@gmail.com>
2020-06-16 09:36:54 -0400

    Add S3 generic and method to match expression with matcher object

---
+++ R/match_expression.R
@@ -0,0 +1,11 @@
+
+#' @export
+match_expression <- function(object, ...) {
+    UseMethod("match_expression")
+}
+
+#' @export
+match_expression.matchr_matcher <- function(object, ...) {
+    exprs <- list(...)
+    lapply(exprs, function(expr) .Call(C_matcher_match_expression, object, expr))
+}
