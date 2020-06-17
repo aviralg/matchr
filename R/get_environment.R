98d11e7e15dd03cd05d5fd0ae4d7cac8ca9a08d0
Aviral Goel <goel.aviral@gmail.com>
2020-06-17 16:18:47 -0400

    Add R API to access pattern environment

--- R/get_environment.R
+++ R/get_environment.R
@@ -5,6 +5,11 @@ get_environment <- function(object, ...) {
 }
 
 #' @export
+get_environment.matchr_pattern <- function(object, ...) {
+    .Call(C_pattern_get_environment, object)
+}
+
+#' @export
 get_environment.matchr_evaluand <- function(object, ...) { # nolint
     .Call(C_evaluand_get_environment, object)
 }
