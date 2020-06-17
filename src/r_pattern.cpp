1d54aa047014c1977187b5c25c1faa74a5d9cfaa
Aviral Goel <goel.aviral@gmail.com>
2020-06-16 22:11:04 -0400

    Raise error if pattern cannot be created

--- src/r_pattern.cpp
+++ src/r_pattern.cpp
@@ -5,8 +5,12 @@ using matchr::Pattern;
 using matchr::PatternSPtr;
 
 SEXP r_pattern_create_pattern(SEXP r_expression) {
-    PatternSPtr pattern =
-        std::shared_ptr<Pattern>(Pattern::create(r_expression));
+    Pattern* pattern_ptr = Pattern::create(r_expression);
+    if (pattern_ptr == nullptr) {
+        /* TODO: add call expression here  */
+        Rf_errorcall(R_NilValue, "unable to create pattern");
+    }
+    PatternSPtr pattern = PatternSPtr(pattern_ptr);
     return Pattern::to_sexp(pattern);
 }
 
